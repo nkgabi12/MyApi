82b19508f54154cf77611063a09ebf3fe999aff0
nkgabi12 <nkgabrielalet23@gmail.com>
2026-07-25 16:28:39 -0300

    Refactor IPTV configuration for multi-provider support
    Updated configuration to support multiple IPTV providers.

--- apitv/config/iptv.php
+++ apitv/config/iptv.php
@@ -1,13 +1,23 @@
 <?php
 /**
- * MovieFlixTV - IPTV Provider Configuration
+ * MovieFlixTV - Multi-Provider IPTV Configuration
  */
 
-define('IPTV_SERVER', 'http://31.43.191.125:8080');
-define('IPTV_USER', 'VIP011651756141415591');
-define('IPTV_PASS', 'fdc0fd87f83a');
+$IPTV_PROVIDERS = [
+    [
+        "name" => "SV1",
+        "server" => "http://31.43.191.125:8080",
+        "user" => "VIP011651756141415591",
+        "pass" => "fdc0fd87f83a"
+    ],
+    [
+        "name" => "SV2",
+        "server" => "http://tv.m3uts.xyz",
+        "user" => "m",
+        "pass" => "m"
+    ]
+];
 
-// Cache settings (to avoid hitting the provider on every request)
-define('IPTV_CACHE_TIME', 3600); // 1 hour
-define('IPTV_CACHE_FILE', __DIR__ . '/../cache/iptv_channels.json');
+define('IPTV_CACHE_TIME', 3600);
+define('IPTV_CACHE_FILE', __DIR__ . '/../cache/iptv_channels_multi.json');
 ?>
