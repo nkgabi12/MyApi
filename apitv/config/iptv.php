482e66757b4db3a0fbb448e81d38fb401821bc09
nkgabi12 <nkgabrielalet23@gmail.com>
2026-08-07 17:49:07 -0300

    Update IPTV configuration and fix syntax issues
    Removed the second IPTV provider configuration and corrected syntax errors.

--- apitv/config/iptv.php
+++ apitv/config/iptv.php
@@ -9,14 +9,7 @@ $IPTV_PROVIDERS = [
         "server" => "http://livetv-plus.com:2082/",
         "user" => "kfS8c",
         "pass" => "VNMAE"
-    ],
-    [
-        "name" => "SV2",
-        "server" => "http://tv.m3uts.xyz",
-        "user" => "m",
-        "pass" => "m"
-    ]
-];
+    ];
 
 define('IPTV_CACHE_TIME', 3600);
 define('IPTV_CACHE_FILE', __DIR__ . '/../cache/iptv_channels_multi.json');
